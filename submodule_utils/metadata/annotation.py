798482e65c69d4d52095f4eb743754475b3d9a93
Amirali Darbandsari <a.darbandsari@ece.ubc.ca>
2020-09-01 04:18:21 -0700

    Fixing Label

--- submodule_utils/metadata/annotation.py
+++ submodule_utils/metadata/annotation.py
@@ -18,27 +18,27 @@ class GroovyAnnotation(object):
         """Get region label from a line in annotation TXT
         """
         return line.split('[')[0].strip()
-    
+
     @classmethod
     def get_vertices(cls, line):
         """Get region segment vertices from a line in annotation TXT
         """
         xy = [float(s) for s in re.findall(cls.POINT_REGEX, line)]
         return [[x,y] for x, y in zip(xy[::2], xy[1::2])]
-    
+
     @classmethod
     def get_path(cls, vertices):
         """Get single region formed by vertices as matplotlib.path.Path
         """
         return matplotlib.path.Path(vertices,
                 closed=True, readonly=True)
-    
+
     @classmethod
     def get_polygon(cls, vertices):
         """Get single region formed by vertices as shapely.geometry.Polygon
         """
         return shapely.geometry.Polygon(vertices)
-    
+
     @classmethod
     def count_polygons_area(cls, polygons):
         return sum(map(lambda p: p.area, polygons))
@@ -54,7 +54,7 @@ class GroovyAnnotation(object):
         self.slide_name = utils.path_to_filename(annotation_file)
         self.annotation_file = annotation_file
         self.__set_up()
-    
+
     def __set_up(self):
         """
         Returns
@@ -67,18 +67,19 @@ class GroovyAnnotation(object):
             for line in f:
                 if line != '':
                     label = self.get_label(line)
-                    if label not in self.paths:
-                        self.paths[label] = []
-                    if label not in self.polygons:
-                        self.polygons[label] = []
-                    vertices = self.get_vertices(line)
-                    self.paths[label].append(self.get_path(vertices))
-                    self.polygons[label].append(self.get_polygon(vertices))
+                    if len(label) != 0:
+                        if label not in self.paths:
+                            self.paths[label] = []
+                        if label not in self.polygons:
+                            self.polygons[label] = []
+                        vertices = self.get_vertices(line)
+                        self.paths[label].append(self.get_path(vertices))
+                        self.polygons[label].append(self.get_polygon(vertices))
 
     def get_area(self, factor=1.0):
         return {label: factor * self.count_polygons_area(polygons) \
                 for label, polygons in self.polygons.items()}
-    
+
     @property
     def labels(self):
         return self.paths.keys()

d48410c706c8154097efb640cf0e02a3fc2bca0f
adarbandsari <a.darbandsari@ece.ubc.ca>
2021-06-23 18:22:50 -0700

    add fake annotation

---
+++ submodule_utils/fake_annotation.py
@@ -0,0 +1,94 @@
+import os
+import cv2
+import numpy as np
+from PIL import Image
+import submodule_utils as utils
+from shapely.geometry import Polygon
+from submodule_utils.metadata.annotation import GroovyAnnotation
+
+
+class FakeAnnotation(object):
+
+    def __init__(self, slide_name, os_slide, annotation_file_path, magnification, patch_size):
+        """
+        Parameters
+        ----------
+        os_slide : OpenSlide
+
+        hd5_file_path : str
+
+        annotation : dict
+
+        """
+        self.patch_size = patch_size
+        self.os_slide = os_slide
+        self.annotation_file_path = annotation_file_path
+        self.slide_name = slide_name
+        self.store_path = os.path.join(os.path.dirname(self.annotation_file_path),
+                                       'Annotation')
+        self.store_thubmnail_path = os.path.join(self.store_path, 'Thumbnail')
+        self.annotation_file = os.path.join(self.store_path, f'{self.slide_name}.txt')
+        self.multi_poly = None
+
+    def add_poly(self, x, y):
+        coords = [(x, y), (x+self.patch_size, y),
+                  (x+self.patch_size, y+self.patch_size), (x, y+self.patch_size)]
+        poly = Polygon(coords)
+
+        if self.multi_poly is None:
+            self.multi_poly = poly
+        else:
+            self.multi_poly = self.multi_poly.union(poly)
+
+    def thumbnail_(self):
+        # Get maximum downsample and minimum dimension
+        self.down_sample = self.os_slide.level_downsamples[-1]
+        self.dimensions  = self.os_slide.level_dimensions[-1]
+
+        self.annotation = GroovyAnnotation(self.annotation_file, 0, 0, False, None)
+
+        def get_thumbnail():
+            thumbnail = self.os_slide.get_thumbnail(self.dimensions)
+            self.thumbnail = cv2.cvtColor(np.array(thumbnail), cv2.COLOR_RGB2BGR)
+
+        def save_thumbnail():
+            self.thumbnail = Image.fromarray(cv2.cvtColor(self.thumbnail, cv2.COLOR_BGR2RGB))
+            self.thumbnail.save(f'{os.path.join(self.store_thubmnail_path, self.slide_name)}.png')
+
+        def draw_annotation():
+            overlay = self.thumbnail.copy()
+            poly = self.annotation.polygons
+            alpha = 0.2 # that's the transparency factor
+            for label, polygons in poly.items():
+                if label=='Tumor': color=(0, 0, 255) # Red # color=(0, 255, 255) # Yellow
+                elif label=='Stroma' or label=='Necrosis': color=(0, 255, 0) # Green # color=(255, 255, 0) # Cyan / Aqua
+                else: color=(192,192,192) # Silver
+                for polygon in polygons:
+                    int_coords = lambda x: (np.array(x)/self.down_sample).round().astype(np.int32)
+                    exterior = [int_coords(polygon.exterior.coords)]
+                    cv2.fillPoly(overlay, exterior, color=color)
+            cv2.addWeighted(overlay, alpha, self.thumbnail, 1 - alpha, 0, self.thumbnail)
+
+        get_thumbnail()
+        draw_annotation()
+        save_thumbnail()
+
+    def save_to_txt_file(self):
+        file = open(self.annotation_file, "w")
+        label = 'Tumor'
+        for poly in self.multi_poly.geoms:
+            line = ''
+            line += label
+            line += ' ['
+            x, y = poly.exterior.coords.xy
+            for x_, y_ in zip(x, y):
+                line += f"Point: {y_}, {x_}, "
+            line = line[:-2] + ']\n'
+            file.write(line)
+        file.close()
+
+    def run(self):
+        os.makedirs(self.store_path, exist_ok=True)
+        os.makedirs(self.store_thubmnail_path, exist_ok=True)
+        self.save_to_txt_file()
+        self.thumbnail_()
